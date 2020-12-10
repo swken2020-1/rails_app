13950de95eb5639b1bbb4b6d091d447db4886423
Ubuntu <ubuntu@ip-172-31-37-229.ec2.internal>
2020-12-10 17:18:28 +0000

    adding user's model error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
-    has_secure_password validations: false
     
-    validates :name, presence: true, uniqueness: true
-    validates :discord_id, presence: true, uniqueness: true
+    validates :name, presence: true, length: { maximum: 20, allow_blank: true }, uniqueness: true
+    validates :discord_id, presence: true, length: { maximum: 20, allow_blank: true }
+    has_secure_password
+    
+    validates :password, presence: true, length: { minimum: 6 }
+#    validates :name, presence: true, uniqueness: true
+#    validates :discord_id, presence: true, uniqueness: true
+#    validates :password_confirmation, presence: true
     
     has_many :recruitments, :dependent => :destroy
 end

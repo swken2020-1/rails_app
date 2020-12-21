d772257a09af742f685fa61e11d02a318ea5972c
Ubuntu <ubuntu@ip-172-31-38-142.ec2.internal>
2020-12-21 14:22:44 +0000

    fix search controller and view

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -40,18 +40,21 @@ class RecruitmentsController < ApplicationController
   end
   
   def search
+    
     if params[:mode]=="指定無し" && params[:want_chara]=="誰でも"
       @recruitments = Recruitment.where(flag: 0).order(id: "DESC")
     elsif params[:mode]=="指定無し"
       @recruitments = Recruitment.where(need_chara: "誰でも", flag: 0).or(Recruitment.where(need_chara: params[:want_chara])).order(id: "DESC")
     elsif params[:want_chara] == "誰でも"
       @recruitments = Recruitment.where(mode: params[:mode], flag: 0).order(id: "DESC")
-    elsif
+    else
       @recruitments = Recruitment.where(mode: params[:mode], need_chara: params[:want_chara], flag: 0).order(id: "DESC")
     end
-    if @recruitments==nil
+    
+    if @recruitments.count==0
       redirect_to recruitments_path, warning: "検索結果にマッチするものがありませんでした"
     end
+    
   end
   
   def edit
